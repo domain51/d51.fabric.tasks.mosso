acc015da1597ea11a29cb44a6eb225ae6e003776
Travis Swicegood <development@domain51.com>
2010-02-02 11:26:58 -0600

    add server creation and deletion

--- d51/fabric/tasks/mosso/servers.py
+++ d51/fabric/tasks/mosso/servers.py
@@ -7,3 +7,19 @@ def list():
 def detail(id=None):
     detail_output(path='servers', id=id, title='Servers', key='server')
 
+def create(name, imageId, flavorId, metadata={}, personality=[]):
+    mosso = get_server_api()
+    response = mosso.servers.POST(
+        name=name,
+        imageId=int(imageId),
+        flavorId=int(flavorId),
+        metadata=metadata,
+        personality=personality
+    )
+
+    result = {"servers": [response['server'], ]}
+    detail_output(result=result, title="New Server", key='server')
+
+def delete(id):
+    mosso = get_server_api()
+    print getattr(mosso.servers, id).DELETE()
